5b6d534b9f678a8526770f91dff0a14dd981539a
Galvon0256 <tanmay.madambi@gmail.com>
2026-04-29 15:57:25 +0530

    Title Module added

--- BookStore/src/main/java/com/cg/repository/PublisherRepository.java
+++ BookStore/src/main/java/com/cg/repository/PublisherRepository.java
@@ -1,7 +1,6 @@
 package com.cg.repository;
 
 import com.cg.entity.Publisher;
-import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.PagingAndSortingRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
